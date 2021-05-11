09316fa43f9c4e9749a87feb58dbd4079bdf385b
Ludovic Lafole <ludovic.lafole@gmail.com>
2021-05-11 14:03:12 +0200

    Display list of ecogestes

--- src/Main.elm
+++ src/Main.elm
@@ -10,14 +10,15 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Http
-import Json.Decode exposing (Decoder, field, string)
+import Http 
+import Json.Decode as JsonDecode exposing (Decoder, string, list)
 
 
 
 -- MAIN
 
 
+main : Program () Model Msg
 main =
   Browser.element
     { init = init
@@ -30,16 +31,16 @@ main =
 
 -- MODEL
 
-
+type alias EcoGestes = List String
 type Model
   = Failure
   | Loading
-  | Success String
+  | Success EcoGestes
 
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  (Loading, getRandomCatGif)
+  (Loading, getEcoGestes)
 
 
 
@@ -47,20 +48,20 @@ init _ =
 
 
 type Msg
-  = MorePlease
-  | GotGif (Result Http.Error String)
+  = FetchEcoGestes
+  | GotEcoGestes (Result Http.Error EcoGestes)
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    MorePlease ->
-      (Loading, getRandomCatGif)
+    FetchEcoGestes ->
+      (Loading, getEcoGestes)
 
-    GotGif result ->
+    GotEcoGestes result ->
       case result of
-        Ok url ->
-          (Success url, Cmd.none)
+        Ok ecoGestes ->
+          (Success ecoGestes, Cmd.none)
 
         Err _ ->
           (Failure, Cmd.none)
@@ -93,31 +94,39 @@ viewGif model =
     Failure ->
       div []
         [ text "I could not load a random cat for some reason. "
-        , button [ onClick MorePlease ] [ text "Try Again!" ]
+        , button [ onClick FetchEcoGestes ] [ text "Try Again!" ]
         ]
 
     Loading ->
       text "Loading..."
 
-    Success url ->
+    Success ecoGestes ->
       div []
-        [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
-        , img [ src url ] []
-        ]
-
+        [ button [ onClick FetchEcoGestes, style "display" "block" ] [ text "More Please!" ]      
+          , renderEcoGeste ecoGestes
+        ] 
+renderList : List String -> Html msg
+renderList lst =
+    ul []
+        (List.map (\l -> li [] [ text l ]) lst)
+        
+renderEcoGeste : EcoGestes -> Html msg
+renderEcoGeste ecogestes =
+  ul []
+    (List.map (\l -> li [] [ text l ]) ecogestes)
+    
 
 
 -- HTTP
 
-
-getRandomCatGif : Cmd Msg
-getRandomCatGif =
+getEcoGestes : Cmd Msg
+getEcoGestes =
   Http.get
-    { url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
-    , expect = Http.expectJson GotGif gifDecoder
+    { url = "/src/data/list.json"
+    , expect = Http.expectJson GotEcoGestes gifDecoder
     }
 
 
-gifDecoder : Decoder String
+gifDecoder : Decoder EcoGestes
 gifDecoder =
-  field "data" (field "image_url" string)
+  JsonDecode.list JsonDecode.string
